2a75acb01d59aa77a339d473a318eda0f09a288e
Jose Picado <jpicado@gmail.com>
2016-12-01 12:52:41 -0800

    Update to problem 3 and working on report.

--- problem_3.m
+++ problem_3.m
@@ -56,12 +56,38 @@ function [  ] = problem_3( )
     end
     
     %Print empirical MSE
+    all_mse_alpha = zeros(alphas, 1);
+    all_p_alpha = zeros(alphas, 1);
+    all_q_alpha = zeros(alphas, 1);
     for a = 1:alphas
         mse_alpha = alpha_squared_error_sum(a,1) / trials;
         mse_p = p_squared_error_sum(a,1) / trials;
         mse_q = q_squared_error_sum(a,1) / trials;
         sprintf('MSEs with alpha = %f: alpha:%f, p:%f, q:%f', real_alpha_vals(a,1), mse_alpha, mse_p, mse_q)
+        
+        all_mse_alpha(a,1) = mse_alpha;
+        all_p_alpha(a,1) = mse_p;
+        all_q_alpha(a,1) = mse_q;
     end
+    
+    figure
+    subplot(3,1,1)
+    plot(real_alpha_vals, all_mse_alpha)
+    xlabel('\alpha')
+    ylabel('MSE(\alpha)')
+%     axis([0.1, 0.9, 0, 1])
+    
+    subplot(3,1,2)
+    plot(real_alpha_vals, all_p_alpha)
+    xlabel('\alpha')
+    ylabel('MSE(p)')
+%     axis([0.1, 0.9, 0, 1])
+
+    subplot(3,1,3)
+    plot(real_alpha_vals, all_q_alpha)
+    xlabel('\alpha')
+    ylabel('MSE(q)')
+%     axis([0.1, 0.9, 0, 1])
     
     
 %     aa = 0.1;
