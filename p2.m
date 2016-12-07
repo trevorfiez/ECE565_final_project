fe0fcdfc83e37c33cfa64b5830359f8cd485c130
trevorfiez <fieztr@oregonstate.edu>
2016-12-07 09:37:42 -0800

    Fixed second derivative

--- p2.m
+++ p2.m
@@ -3,10 +3,11 @@ function [ prob ] = p2( k1, p, n )
 %   Detailed explanation goes here
     b = nchoosek(n, k1);
     
+    %prob = k1 * (k1 - 1) * p ^ (k1 - 2) * (1 - p) ^ (n - k1);
+    %prob = prob + 2 * (n - k1) * k1 * p ^ (k1 - 1) * (1 - p) ^ (n - k1 - 1);
+    %prob = prob + (n - k1) * (n - k1 - 1) * p ^ k1 * (1 - p) ^ (n - k1 - 2);
     prob = k1 * (k1 - 1) * p ^ (k1 - 2) * (1 - p) ^ (n - k1);
-    prob = prob + 2 * (n - k1) * k1 * p ^ (k1 - 1) * (1 - p) ^ (n - k1 - 1);
-    prob = prob + (n - k1) * (n - k1 - 1) * p ^ k1 * (1 - p) ^ (n - k1 - 2);
-    
+    prob = prob - (-k1 + n - 1)*(n - k1)*p^k1 *(1 - p)^(-k1 + n - 2);
     prob = prob * b;
 
 end
